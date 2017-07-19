6c83ce761d592ef01ed1a9daa8b707062202a6e5
karan <karangupta.dev@gmail.com>
2017-07-19 13:13:27 +0530

    added params for login() and signout()

--- src/main/java/com/linksharing/model/services/interfaces/UserService.java
+++ src/main/java/com/linksharing/model/services/interfaces/UserService.java
@@ -1,9 +1,10 @@
 package com.linksharing.model.services.interfaces;
 
+import com.linksharing.dto.LoginDTO;
 import com.linksharing.dto.UserDTO;
-import com.linksharing.model.entities.User;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpSession;
 import java.util.Map;
 
 /**
@@ -11,9 +12,10 @@ import java.util.Map;
  */
 public interface UserService {
 
-   void login();
+   int login(LoginDTO user, HttpSession session);
    Map<String, String> register(UserDTO user, MultipartFile file);
-   void signout();
+   void signout(HttpSession session);
    void forgotPassword();
    void updateProfileDetails();
+
 }
