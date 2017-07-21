d982dbb586a35aec188b221d8d65e63191ce4aa3
karan <karangupta.dev@gmail.com>
2017-07-21 23:51:58 +0530

    do not commit

--- src/main/java/com/linksharing/util/DBtest.java
+++ src/main/java/com/linksharing/util/DBtest.java
@@ -11,6 +11,19 @@ public class DBtest {
 
     @Autowired
     SessionFactory sessionFactory;
+<<<<<<< Updated upstream
+=======
+    private static final Logger logger = Logger.getLogger(DBtest.class);
+
+
+    @RequestMapping(value={"test"}, method = RequestMethod.GET)
+    public String getRecentPublicTopics(Model model){
+        Session session = sessionFactory.openSession();
+        Query query = session.createQuery("FROM Resource r WHERE r.topic.visibility=0 ORDER BY r.lastUpdated DESC").setMaxResults(5);
+        List<Object> topicList =query.list();
+        model.addAttribute("topicList",topicList);
+        session.close();
+>>>>>>> Stashed changes
 
 
 
